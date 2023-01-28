6c692d38ac4e4273282472c582a5faf68dd82530
Jacky Park <jackypark9852@gmail.com>
2023-01-28 01:05:41 -0600

    Add roll speed scaling based on ball radius

--- Assets/Scripts/Snowball/RollingSpeedController.cs
+++ Assets/Scripts/Snowball/RollingSpeedController.cs
@@ -3,31 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SphereCollider))]
 public class RollingSpeedController : MonoBehaviour
 {
-    private Vector3 _lastPosition;
     private Animator _animator;
+    private SphereCollider _collider; // The collider of the snowball
+    private float _radius;
+    private Vector3 _lastPosition;
     // Start is called before the first frame update
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _collider = GetComponent<SphereCollider>();
     }
 
     private void Start()
     {
-        _lastPosition = transform.position;
+        UpdateLastPosition();
+        UpdateRadius();
     }
 
     private void Update()
     {
+        UpdateRadius();
         float speed = GetSpeed();
-        _animator.speed = speed; // Set the speed parameter in the animator so that snowball rolls at correct speed
-        _lastPosition = transform.position;
+        _animator.speed = speed / _radius; // Set the speed parameter in the animator so that snowball rolls at correct speed
+        UpdateLastPosition();
     }
-
-
     private float GetSpeed()
     {
         return (transform.position - _lastPosition).magnitude / Time.deltaTime;
+    }
+
+    private void UpdateRadius()
+    {
+        _radius = transform.lossyScale.x * _collider.radius; // Calculate the radius of the snowball using global scale and local radius
+    }
+
+    private void UpdateLastPosition()
+    {
+        _lastPosition = transform.position;
     }
 }
